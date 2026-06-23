382aff731ac4e322822f4681749ab35c9f854368
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-06-23 22:47:07 +1000

    Installed nanoid, created notes page

--- src/mainview/routes/index.tsx
+++ src/mainview/routes/index.tsx
@@ -1,15 +1,26 @@
 import { Button } from "@/components/ui/button";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { nanoid } from "nanoid";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const router = useRouter();
+
   return (
     <div>
       <p>Homepage</p>
-      <Button>New note</Button>
+      <Button
+        onClick={() =>
+          router.navigate({
+            to: `/notes/${nanoid()}`,
+          })
+        }
+      >
+        New note
+      </Button>
     </div>
   );
 }
---
+++ src/mainview/routes/notes/$noteId.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/notes/$noteId")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { noteId } = Route.useParams();
+
+  return <div>Hello `/notes/${noteId}`!</div>;
+}
