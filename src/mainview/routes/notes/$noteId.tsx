836ffc780234e93efa650ec386500da5f43050c9
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-06-23 22:51:07 +1000

    Block editor added to note page

--- src/mainview/routes/notes/$noteId.tsx
+++ src/mainview/routes/notes/$noteId.tsx
@@ -1,4 +1,8 @@
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/shadcn";
 import { createFileRoute } from "@tanstack/react-router";
+import "@blocknote/shadcn/style.css";
+import { useTheme } from "@/components/theme-provider";
 
 export const Route = createFileRoute("/notes/$noteId")({
   component: RouteComponent,
@@ -6,6 +10,35 @@ export const Route = createFileRoute("/notes/$noteId")({
 
 function RouteComponent() {
   const { noteId } = Route.useParams();
+  const { resolvedTheme } = useTheme();
+  const editor = useCreateBlockNote({
+    initialContent: [
+      { type: "heading", content: noteId },
+      {
+        type: "paragraph",
+        content: "Welcome to this demo!",
+      },
+      {
+        type: "paragraph",
+        content: "Open up a menu or toolbar to see more of the red theme",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Toggle light/dark mode in the page footer and see the theme change too",
+      },
+    ],
+  });
 
-  return <div>Hello `/notes/${noteId}`!</div>;
+  return (
+    <div>
+      <div>Hello `/notes/${noteId}`!</div>
+
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme}
+        data-theming-css-variables
+      />
+    </div>
+  );
 }
